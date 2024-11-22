f005c69e5d367dd83756ff544d4dd1c950d5f819
IgorB-2A <00001109488890sp@al.educacao.sp.gov.br>
2024-11-22 19:28:43 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,19 @@
 criaCartao(
     'Programação',
     'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O Python é uma linguagem de programação amplamente usada em aplicações da Web, desenvolvimento de software, ciência de dados e machine learning (ML). Os desenvolvedores usam o Python porque é eficiente e fácil de aprender e pode ser executada em muitas plataformas diferentes.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O que é a ONU?',
+    'A Organização das Nações Unidas (ONU) é uma organização internacional fundada em 1945 para intermediar as relações internacionais, harmonizar a ação das nações diante de objetivos comuns, atuar para o desenvolvimento mundial e garantir a paz.'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é JavaScript',
+    'JavaScript é uma poderosa linguagem de programação que pode adicionar interatividade a um site. Foi inventado por Brendan Eich. JavaScript é versátil e amigável para iniciantes. Com mais experiência, você poderá criar jogos, gráficos 2D e 3D animados, aplicativos abrangentes baseados em banco de dados.'
 )
 
 criaCartao(
